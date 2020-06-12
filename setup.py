d224ca606dd2e8a10ca42d3870760ce23ee034b2
alexdelorenzo <you@example.com>
2020-06-11 23:31:18 -0400

    Refactor and increment version

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
+__author__ = "Alex DeLorenzo"
 from setuptools import setup
 from pathlib import Path
 
+
+NAME = "chromecast_mpris"
+VERSION = "0.1.4"
+LICENSE = "AGPL-3.0"
 
 requirements = \
   Path('requirements.txt') \
@@ -9,18 +14,18 @@ requirements = \
 readme = Path('README.md').read_text()
 
 setup(
-      name="chromecast_mpris",
-      version="0.1.3",
+      name=NAME,
+      version=VERSION,
       description="Control Chromecasts via MPRIS.",
       long_description=readme,
       long_description_content_type="text/markdown",
       url="https://alexdelorenzo.dev",
-      author="Alex DeLorenzo",
-      license="AGPL-3.0",
-      packages=['chromecast_mpris'],
+      author=__author__,
+      license=LICENSE,
+      packages=[NAME],
       zip_safe=True,
       install_requires=requirements,
       entry_points={"console_scripts":
-                      ["chromecast_mpris = chromecast_mpris.command:cmd"]},
+                      [f"{NAME} = {NAME}.command:cmd"]},
       python_requires='>=3.6',
 )
