6b3797801d8f51036f10dd8ee7878f1227c23372
alexdelorenzo <you@example.com>
2021-05-23 19:34:00 -0400

    Remove new CLI

--- chromecast_mpris/command.py
+++ chromecast_mpris/command.py
@@ -17,14 +17,8 @@ Control casting devices via Linux media controls and desktops.
 This daemon connects your casting device directly to the D-Bus media player interface.
 """
 
-
-@click.group(help=HELP)
-def cmd():
-  pass
-
-
 @cmd.command(
-  help='Run the service locally.',
+  help=HELP
 )
 @click.option('--name', '-n',
   default=None, show_default=True, type=click.STRING,
@@ -47,7 +41,7 @@ def cmd():
 @click.option('--log-level', '-l',
   default=LOG_LEVEL, show_default=True, type=click.STRING,
   help='Set the debugging log level.')
-def run(
+def cmd(
   name: Optional[str],
   host: Optional[str],
   uuid: Optional[str],
@@ -66,80 +60,6 @@ def run(
     log_level
   )
 
-
-@cmd.group(
-  help='Start, stop or restart the background service.'
-)
-def service():
-  pass
-
-
-@service.command(
-  help='Run the service in the background.'
-)
-@click.option('--name', '-n',
-  default=None, show_default=True, type=click.STRING,
-  help="Connect to a device via its name, otherwise control the first device found.")
-@click.option('--host', '-h',
-  default=None, show_default=True, type=click.STRING,
-  help="Connect to a device via its hostname or IP address, otherwise control the first device found.")
-@click.option('--uuid', '-u',
-  default=None, show_default=True, type=click.STRING,
-  help="Connect to a device via its UUID, otherwise control the first device found.")
-@click.option('--wait', '-w',
-  default=None, show_default=True, type=click.FLOAT,
-  help="Seconds to wait between trying to make initial successful connections to a device.")
-@click.option('--retry-wait', '-r',
-  default=DEFAULT_RETRY_WAIT, show_default=True, type=click.FLOAT,
-  help="Seconds to wait between reconnection attempts if a successful connection is interrupted.")
-@click.option('--icon', '-i',
-  is_flag=True, default=False, show_default=True, type=click.BOOL,
-  help="Use a lighter icon instead of the dark icon. The lighter icon goes well with dark themes.")
-@click.option('--log-level', '-l',
-  default=LOG_LEVEL, show_default=True, type=click.STRING,
-  help='Set the debugging log level.')
-def run(
-  name: Optional[str],
-  host: Optional[str],
-  uuid: Optional[str],
-  wait: Optional[float],
-  retry_wait: Optional[float],
-  icon: bool,
-  log_level: str
-):
-  daemon = get_daemon(
-    run_safe,
-    name,
-    host,
-    uuid,
-    wait,
-    retry_wait,
-    icon,
-    log_level
-  )
-
-  daemon.start()
-
-
-@service.command(
-  help='Stop the background service.'
-)
-def stop():
-  daemon = get_daemon()
-  daemon.stop()
-
-
-@service.command(
-  help='Restart the background service.'
-)
-def restart():
-  daemon = get_daemon()
-
-  if not daemon.pid:
-    logging.warn("Daemon isn't running.")
-    sys.exit(RC_NOT_RUNNING)
-
-  daemon.restart()
 
 
 if __name__ == "__main__":
