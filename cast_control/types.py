acacfb4c994cc01ca6845ff436f784045c5a0b91
alexdelorenzo <alex@alexdelorenzo.dev>
2021-06-02 04:52:33 -0400

    Add comments

--- cast_control/types.py
+++ cast_control/types.py
@@ -1,5 +1,7 @@
+# Just import Protocol here so this pattern doesn't
+# need to be used more than once
 try:
-  from typing import Protocol
+  from typing import Protocol  # Python 3.8+
 
 except ImportError:
-  from typing_extensions import Protocol
+  from typing_extensions import Protocol  # Python 3.7
