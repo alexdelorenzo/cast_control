7e3c87de4815913e4145610c05227c6f363110c9
alexdelorenzo <alex@alexdelorenzo.dev>
2021-06-04 18:55:37 -0400

    Import `Final` from `typing` or backported `typing_extensions`

--- cast_control/types.py
+++ cast_control/types.py
@@ -1,9 +1,12 @@
-# Just import Protocol here so this pattern doesn't
-# need to be used more than once
+# just import newer `typing` features here so this pattern
+# isn't repeated throughout the project.
+
+# Protocol, Final, etc are not available on Python 3.7
 try:
   # Python 3.8+
-  from typing import Protocol, runtime_checkable
+  from typing import Protocol, runtime_checkable, Final
 
 except ImportError:
   # Python 3.7
-  from typing_extensions import Protocol, runtime_checkable
+  from typing_extensions import \
+    Protocol, runtime_checkable, Final
