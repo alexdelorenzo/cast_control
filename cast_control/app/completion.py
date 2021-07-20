4c6ab1cd9c6010c509373564a279175efa89a789
alexdelorenzo <alex@alexdelorenzo.dev>
2021-07-19 23:16:55 -0400

    Refactor shell completions

--- cast_control/app/completion.py
+++ cast_control/app/completion.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
-from typing import Iterable
+from typing import Iterable, Optional
 from subprocess import run
 from enum import auto
 from pathlib import Path
 from functools import cache
+from abc import ABC, abstractmethod
 
 from strenum import StrEnum
 
@@ -13,108 +14,96 @@ from . import NAME, SHORT_NAME
 NAMES: tuple[str] = NAME, SHORT_NAME
 
 
-class Shell(StrEnum):
+class ShellName(StrEnum):
   bash: str = auto()
   fish: str = auto()
   zsh: str = auto()
 
 
-SRC_CMDS: dict[Shell, str] = {
-  Shell.bash: 'bash_source',
-  Shell.fish: 'fish_source',
-  Shell.zsh: 'zsh_source'
-}
+class Shell(ABC):
+  name: ShellName
+  src_cmd: str
+  script_ext: str
+  config: Optional[Path] = None
+
+  @abstractmethod
+  def create_completions(self):
+    pass
+
+  @abstractmethod
+  def get_completion_path(self, app_name: str) -> Path:
+    pass
+
+  def gen_completions(self) -> Iterable[Path]:
+    for app_name in NAMES:
+      file = self.get_completion_path(app_name)
+
+      cmd = self.get_cmd(self.name, app_name)
+      shell_cmd = f'{cmd} > {file}'
+      run(shell_cmd, shell=True)
+
+      yield file
+
+  def get_cmd(self, name: str) -> str:
+    caps = name.upper()
+
+    return f'_{caps}_COMPLETE={self.src_cmd} {name}'
 
 
-EXTS: dict[Shell, str] = {
-  Shell.bash: 'sh',
-  Shell.fish: 'fish',
-  Shell.zsh: 'zsh'
-}
+class Bash(Shell):
+  name = ShellName.bash
+  src_cmd: str = 'bash_src'
+  script_ext: str = 'sh'
+  config: str = Path('~/.bashrc')
+
+  def get_completion_path(self, app_name: str) -> Path:
+    path = f'~/.config/{app_name}-complete.{self.script_ext}'
+    return Path(path)
+
+  def create_completions(self):
+    for path in self.gen_completions():
+      line = f'. {path}'
+      add_line_to_file(line, self.config)
 
 
-CFGS: dict[Shell, Path] = {
-  Shell.bash: Path('~/.bashrc'),
-  Shell.zsh: Path('~/.zshrc'),
-}
+class Fish(Shell):
+  name = ShellName.fish
+  src_cmd: str = 'fish_src'
+  script_ext: str = 'fish'
+
+  def get_completion_path(self, app_name: str) -> Path:
+    path = f'~/.config/fish/completions/{app_name}.{self.script_ext}'
+    return Path(path)
+
+  def create_completions(self):
+    for _ in self.gen_completions():
+      pass
 
 
-def get_cmd(shell: Shell, name: str) -> str:
-  src_cmd = SRC_CMDS[shell]
-  caps = name.upper()
+class Zsh(Shell):
+  name = ShellName.zsh
+  src_cmd: str = 'zsh_src'
+  script_ext: str = 'zsh'
+  config: str = Path('~/.zshrc')
 
-  return f"_{caps}_COMPLETE={src_cmd} {name}"
+  def get_completion_path(self, app_name: str) -> Path:
+    path = f'~/.config/{app_name}-complete.{self.script_ext}'
+    return Path(path)
 
-
-def gen_fish_completions() -> Iterable[Path]:
-  shell = Shell.fish
-  ext = EXTS[shell]
-
-  for name in NAMES:
-    file = Path(f"~/.config/fish/completions/{name}.{ext}")
-
-    cmd = get_cmd(shell, name)
-    shell_cmd = f"{cmd} > {file}"
-    run(shell_cmd, shell=True)
-
-    yield file
-
-
-def gen_bash_completions() -> Iterable[Path]:
-  shell = Shell.bash
-  ext = EXTS[shell]
-
-  for name in NAMES:
-    file = Path(f"~/.config/{name}-complete.{ext}")
-
-    cmd = get_cmd(shell, name)
-    shell_cmd = f"{cmd} > {file}"
-    run(shell_cmd, shell=True)
-
-    yield file
-
-
-def gen_zsh_completions() -> Iterable[Path]:
-  shell = Shell.zsh
-  ext = EXTS[shell]
-
-  for name in NAMES:
-    file = Path(f"~/.config/{name}-complete.{ext}")
-
-    cmd = get_cmd(shell, name)
-    shell_cmd = f"{cmd} > {file}"
-    run(shell_cmd, shell=True)
-
-    yield file
+  def create_completions(self):
+    for path in self.gen_completions():
+      line = f'. {path}'
+      add_line_to_file(line, self.config)
 
 
 @cache
 def add_line_to_file(line: str, path: Path):
-  text = path.read_text()
+  # check if line exists
+  with path.open(mode='r') as file:
+    for text in file:
+      if line in text:
+        return
 
-  if line in text:
-    return
-
+  # write line
   with path.open(mode='a') as file:
     file.write(line)
-
-
-def do_fish():
-  for _ in gen_fish_completions():
-    pass
-
-
-def do_bash():
-  cfg = CFGS[Shell.bash]
-
-  for path in gen_bash_completions():
-    line = f". {path}"
-    add_line_to_file(line, cfg)
-
-
-def do_zsh():
-  cfg = CFGS[Shell.zsh]
-
-  for path in gen_zsh_completions():
-    line = f". {path}"
-    add_line_to_file(line, cfg)
