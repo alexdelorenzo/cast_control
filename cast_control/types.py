0fd52e68593390414168094671b4538220e9f9dc
Alex DeLorenzo <alex@alexdelorenzo.dev>
2022-06-22 19:58:28 -0400

    Fix 3.11 support

--- cast_control/types.py
+++ cast_control/types.py
@@ -1,4 +1,8 @@
 # just import newer `typing` features here so this pattern
 # isn't repeated throughout the project.
 
-from typing_extensions import Protocol, runtime_checkable, Final
+try:
+    from typing import Protocol, runtime_checkable, Final
+
+except ImportError:
+    from typing_extensions import Protocol, runtime_checkable, Final
