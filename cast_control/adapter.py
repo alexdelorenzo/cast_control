d20b109c93d04dc92fa33d8dce760a1d7f46b322
alexdelorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2023-11-02 03:43:00 -0400

    Decorate overrides

--- cast_control/adapter.py
+++ cast_control/adapter.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Optional
+from typing import Optional, override
 
 from mpris_server import (
   Metadata, MprisAdapter, PlayState, PlayerAdapter, Rate, RootAdapter,
@@ -23,65 +23,85 @@ class DeviceIntegration(Protocol):
 
 
 class DeviceRootAdapter(DeviceIntegration, RootAdapter):
+  @override
   def get_uri_schemes(self) -> list[str]:
     return URI
 
+  @override
   def get_mime_types(self) -> list[str]:
     return MIME_TYPES
 
+  @override
   def get_desktop_entry(self) -> str:
     return self.wrapper.get_desktop_entry()
 
+  @override
   def can_quit(self) -> bool:
     return self.wrapper.can_quit()
 
+  @override
   def quit(self):
     self.wrapper.quit()
 
 
 class DevicePlayerAdapter(DeviceIntegration, PlayerAdapter):
+  @override
   def can_go_next(self) -> bool:
     return self.wrapper.can_play_next()
 
+  @override
   def can_go_previous(self) -> bool:
     return self.wrapper.can_play_prev()
 
+  @override
   def can_play(self) -> bool:
     return self.wrapper.can_play()
 
+  @override
   def can_pause(self) -> bool:
     return self.wrapper.can_pause()
 
+  @override
   def can_seek(self) -> bool:
     return self.wrapper.can_seek()
 
+  @override
   def can_control(self) -> bool:
     return self.wrapper.can_control()
 
+  @override
   def get_current_position(self) -> Microseconds:
     return self.wrapper.get_current_position()
 
+  @override
   def next(self):
     self.wrapper.next()
 
+  @override
   def previous(self):
     self.wrapper.previous()
 
+  @override
   def pause(self):
     self.wrapper.pause()
 
+  @override
   def resume(self):
     self.play()
 
+  @override
   def stop(self):
     self.wrapper.stop()
 
+  @override
   def play(self):
     self.wrapper.play()
 
+  @override
   def get_playstate(self) -> PlayState:
     return self.wrapper.get_playstate()
 
+  @override
   def seek(
     self,
     time: Microseconds,
@@ -89,54 +109,71 @@ class DevicePlayerAdapter(DeviceIntegration, PlayerAdapter):
   ):
     self.wrapper.seek(time)
 
+  @override
   def open_uri(self, uri: str):
     self.wrapper.open_uri(uri)
 
+  @override
   def is_repeating(self) -> bool:
     return self.wrapper.is_repeating()
 
+  @override
   def is_playlist(self) -> bool:
     return self.wrapper.is_playlist()
 
+  @override
   def get_rate(self) -> Rate:
     return self.wrapper.get_rate()
 
+  @override
   def set_rate(self, val: Rate):
     pass
 
+  @override
   def get_shuffle(self) -> bool:
     return False
 
+  @override
   def set_shuffle(self, val: bool):
     pass
 
+  @override
   def get_art_url(self, track: int = None) -> str:
     return self.wrapper.get_art_url(track)
 
+  @override
   def get_volume(self) -> Volume:
     return self.wrapper.get_volume()
 
+  @override
   def set_volume(self, val: Volume):
     self.wrapper.set_volume(val)
 
+  @override
   def is_mute(self) -> bool:
     return self.wrapper.is_mute()
 
+  @override
   def set_mute(self, val: bool):
     self.wrapper.set_mute(val)
 
+  @override
   def get_stream_title(self) -> str:
     return self.wrapper.get_stream_title()
 
+  @override
   def get_duration(self) -> Microseconds:
     return self.wrapper.get_duration()
 
+  @override
   def metadata(self) -> Metadata:
     return self.wrapper.metadata()
 
+  @override
   def get_current_track(self) -> Track:
     return self.wrapper.get_current_track()
 
+  @override
   def add_track(
     self,
     uri: str,
@@ -145,18 +182,23 @@ class DevicePlayerAdapter(DeviceIntegration, PlayerAdapter):
   ):
     self.wrapper.add_track(uri, after_track, set_as_current)
 
+  @override
   def can_edit_track(self) -> bool:
     return self.wrapper.can_edit_track()
 
+  @override
   def set_repeating(self, val: bool):
     pass
 
+  @override
   def set_loop_status(self, val: str):
     pass
 
+  @override
   def get_previous_track(self) -> Track:
     pass
 
+  @override
   def get_next_track(self) -> Track:
     pass
 
