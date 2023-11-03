fabc1208cbf91e121c5d84ce11e64b3921e66aaa
alexdelorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2023-11-02 23:24:18 -0400

    Conform to param naming

--- cast_control/adapter.py
+++ cast_control/adapter.py
@@ -15,8 +15,8 @@ from .device.wrapper import DeviceWrapper
 class DeviceIntegration(Protocol):
   wrapper: DeviceWrapper
 
-  def set_icon(self, light_icon: bool):
-    self.wrapper.set_icon(light_icon)
+  def set_icon(self, lighter: bool):
+    self.wrapper.set_icon(lighter)
 
   def on_new_status(self, *args, **kwargs):
     self.wrapper.on_new_status(*args, **kwargs)
